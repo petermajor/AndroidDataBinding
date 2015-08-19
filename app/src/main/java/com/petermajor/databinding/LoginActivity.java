829598d6b4961d6650ab09a900407a0971fe6d40
Peter Major <peter@petermajor.co.uk>
2015-08-19 22:47:26 +0100

    Open the keyboard

--- app/src/main/java/com/petermajor/databinding/LoginActivity.java
+++ app/src/main/java/com/petermajor/databinding/LoginActivity.java
@@ -3,6 +3,7 @@ package com.petermajor.databinding;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import com.petermajor.databinding.databinding.ActivityLoginBinding;
 
@@ -17,6 +18,8 @@ public class LoginActivity extends AppCompatActivity {
         viewModel = new LoginViewModel();
         viewModel.setUsername("peter@petermajor.co.uk");
         viewModel.setPassword("Passw0rd");
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
         final ActivityLoginBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         binding.setViewModel(viewModel);
