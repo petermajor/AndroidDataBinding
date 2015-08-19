2c283ef8a242b1db3f8de6edb1855d78a2c5b81b
Peter Major <peter@petermajor.co.uk>
2015-08-19 08:39:11 +0100

    Add watcher on EditText for "two-way' binding

--- app/src/main/java/com/petermajor/databinding/LoginViewModel.java
+++ app/src/main/java/com/petermajor/databinding/LoginViewModel.java
@@ -2,12 +2,14 @@ package com.petermajor.databinding;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.text.TextWatcher;
 import android.util.Log;
 
 public class LoginViewModel extends BaseObservable {
 
     private String username;
     private String password;
+    private boolean isInNotification = false;
 
     private Command loginCommand;
 
@@ -33,12 +35,71 @@ public class LoginViewModel extends BaseObservable {
     public Command getLoginCommand() { return loginCommand; }
 
     public void setUsername(String username) {
+        if (stringEquals(this.username, username))
+            return;
+
         this.username = username;
-        notifyPropertyChanged(com.petermajor.databinding.BR.username);
+
+        if (!isInNotification)
+            notifyPropertyChanged(com.petermajor.databinding.BR.username);
     }
 
     public void setPassword(String password) {
+        if (stringEquals(this.password, password))
+            return;
+
         this.password = password;
-        notifyPropertyChanged(com.petermajor.databinding.BR.password);
+
+        if (!isInNotification)
+            notifyPropertyChanged(com.petermajor.databinding.BR.password);
+    }
+
+    @Bindable
+    public TextWatcher getOnUsernameChanged() {
+
+        return new SimpleTextWatcher() {
+            @Override
+            public void onTextChanged(String newValue) {
+                isInNotification = true;
+                setUsername(newValue);
+                isInNotification = false;
+            }
+        };
+    }
+
+    @Bindable
+    public TextWatcher getOnPasswordChanged() {
+
+        return new SimpleTextWatcher() {
+            @Override
+            public void onTextChanged(String newValue) {
+                isInNotification = true;
+                setPassword(newValue);
+                isInNotification = false;
+            }
+        };
+    }
+//    @BindingAdapter("bind:onTextChanged")
+//    public static void setOnTextChanged(EditText view, final OnTextChanged changed) {
+//
+//        TextWatcher watcher = new TextWatcher() {
+//
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//            }
+//
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//            }
+//
+//            public void afterTextChanged(Editable s) {
+//                changed.onTextChanged(s.toString());
+//            }
+//        };
+//
+//        view.addTextChangedListener(watcher);
+//    }
+
+
+    static boolean stringEquals(String str1, String str2) {
+        return (str1 == null ? str2 == null : str1.equals(str2));
     }
 }
